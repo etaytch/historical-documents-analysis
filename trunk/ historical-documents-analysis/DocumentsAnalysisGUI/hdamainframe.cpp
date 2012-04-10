a6882e2545c2a3836851f6b854f1288b462c827d
vadipo@gmail.com <vadipo@gmail.com@4f72bd24-de06-9c6a-47bd-b6014c8e3906>
2012-04-10 17:19:21 +0000

    GUI draft

---
+++ trunk/ historical-documents-analysis/DocumentsAnalysisGUI/hdamainframe.cpp
@@ -0,0 +1,12 @@
+#include "hdamainframe.h"
+
+HdaMainFrame::HdaMainFrame(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui.setupUi(this);
+}
+
+HdaMainFrame::~HdaMainFrame()
+{
+
+}
