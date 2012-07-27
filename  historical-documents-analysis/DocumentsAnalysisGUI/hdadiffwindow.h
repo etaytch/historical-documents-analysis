aaf18b361082fe8765a3d0e8b38d714877b4cfe2
gafniv@gmail.com <gafniv@gmail.com@4f72bd24-de06-9c6a-47bd-b6014c8e3906>
2012-07-27 05:39:28 +0000

    enable markers in HdaDiffWindow. wrong position

---  historical-documents-analysis/DocumentsAnalysisGUI/hdadiffwindow.h
+++  historical-documents-analysis/DocumentsAnalysisGUI/hdadiffwindow.h
@@ -2,8 +2,12 @@
 #define HDADIFFWINDOW_H
 
 #include <QWidget>
+#include <QMouseEvent>
+#include <QPainter>
 #include "ui_hdadiffwindow.h"
 #include "PageDoc.h"
+
+enum ChooseState {CHOOSE_NONE, CHOOSE_START,CHOOSE_END};
 
 class HdaDiffWindow : public QDialog
 {
@@ -12,14 +16,27 @@ class HdaDiffWindow : public QDialog
 public:
 	HdaDiffWindow(QWidget *parent = 0);
 	HdaDiffWindow(PageDoc page1, PageDoc page2, QWidget *parent = 0);
+	
 	~HdaDiffWindow();
-
+ public slots:
+    void startPressed();
+	void endPressed();
+	void comparePressed();
 private:
 	Ui::HdaDiffWindow ui;
 	PageDoc _page1;
 	PageDoc _page2;
 	QPixmap _pic1;
 	QPixmap _pic2;
+	QPixmap _orig_pic1;
+	QPixmap _orig_pic2;
+	ChooseState _chooseState;
+	QPoint _StartPos1;
+	QPoint _StartPos2;
+	QPoint _EndPos1;
+	QPoint _EndPos2;
+protected:
+	 bool eventFilter(QObject *obj, QEvent *ev);
 
 };
 
