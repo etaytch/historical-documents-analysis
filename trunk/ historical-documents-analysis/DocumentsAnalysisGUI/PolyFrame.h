cf7a2e07ada5ddda24046f7cb7ef016ebbbde61d
yair87@gmail.com <yair87@gmail.com@4f72bd24-de06-9c6a-47bd-b6014c8e3906>
2012-08-25 08:03:16 +0000

    comments added.

--- trunk/ historical-documents-analysis/DocumentsAnalysisGUI/PolyFrame.h
+++ trunk/ historical-documents-analysis/DocumentsAnalysisGUI/PolyFrame.h
@@ -7,6 +7,12 @@
 #include <QPolygonF>
 #include <algorithm>
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/// <summary>	Polygon Frame. </summary>
+///
+/// <remarks>	Yair Offir, 1/8/2012. </remarks>
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class PolyFrame : public FrameDraw
 {
 	Q_OBJECT
@@ -23,6 +29,7 @@ public:
 	virtual QRectF boundingRect() const; 
     virtual void paint (QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget); 
 	virtual void mouseMoveEvent ( QGraphicsSceneMouseEvent * event );
+	//if polygon is out of the scene, move it back to the scene.
 	QPointF adjustToScene(float x, float y);
     virtual void mouseMoveEvent(QGraphicsSceneDragDropEvent *event);
     virtual void mousePressEvent (QGraphicsSceneMouseEvent * event );
@@ -30,9 +37,13 @@ public:
     virtual void mouseReleaseEvent (QGraphicsSceneMouseEvent * event );
 	virtual void hoverEnterEvent ( QGraphicsSceneHoverEvent * event ); 
     virtual void hoverLeaveEvent ( QGraphicsSceneHoverEvent * event ); 
+	//polygon type is 2.
 	virtual int getType();
+	//gets a QVector with the polygon points
 	QVector<QPointF> getPoints();
+	//get the closest Polygon point to the mouseOnPoint point.
 	int getClosestPoint(QPointF mouseOnPoint);
+	//measures the distance between 2 points.
 	int distance(QPointF p1, QPointF p2);
 	int movingIndex;
 	~PolyFrame(void);
